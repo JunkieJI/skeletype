f7423d11420498973180d29c7399678f259f1a66
Jihaad Pienaar <jihaad.pienaar@spandigital.com>
2018-07-21 16:27:49 +0200

    Batch of work adding the different sprites and game layouts. Also began work on animation controllers

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+	[SerializeField] float speed = 0.5f;
+	Rigidbody2D enemyBody;
+
+	Animator animator;
+
+	bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+		enemyBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!isDead) {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		}	
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		switch(col.gameObject.name) {
+			case "rocket":
+				animator.SetBool("isDying", true);
+				isDead = true;
+				break;
+			case "crate":
+				break;
+		}
+	}
+}
---
+++ Assets/scripts/rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+	[SerializeField] float speed = -0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("start rocket");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.right * speed * Time.deltaTime);		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+    }
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log("OnTriggerEnter2D");
+	}
+}
